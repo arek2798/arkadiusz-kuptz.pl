153b3ffd353edcb9dae6efdaa1211d3b25ca01e6
Arkadiusz Kuptz <arekk2798@outlook.com>
2020-03-01 00:31:20 +0100

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
